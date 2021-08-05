74e03f0768c9510ff1699a36b4e3c260a25bd5e3
Junaid <mohdjunaid.77@gmail.com>
2021-08-05 12:32:16 +0530

    session manager

--- app/src/main/java/com/example/mvvmsample/sessionManager/SessionManager.java
+++ app/src/main/java/com/example/mvvmsample/sessionManager/SessionManager.java
@@ -1,4 +1,23 @@
 package com.example.mvvmsample.sessionManager;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class SessionManager {
+    private SharedPreferences pref;
+    // Editor reference for Shared preferences
+    private SharedPreferences.Editor editor;
+
+    private static final String PREFER_NAME = "MVVMPref";
+
+    // Context
+    private Context _context;
+    // Shared pref mode
+    private int PRIVATE_MODE = 0;
+
+    public SessionManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREFER_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
 }
